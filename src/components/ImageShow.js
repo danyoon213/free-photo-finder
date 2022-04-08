31a52fe8e4f015433a799cd7598869a68e1720f7
Daniel Yoon <danyoonsignup@gmail.com>
2022-04-08 11:39:06 -0700

    CSS Grid responds to Img Height
    The React app checks each img height and changes the "span height" accordingly for CSS grid

---
+++ src/components/ImageShow.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+class ImageShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setGridSpan);
+  }
+
+  setGridSpan = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans: spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
+}
+
+export default ImageShow;
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,11 +1,13 @@
+import "./Images.css";
 import React from "react";
+import ImageShow from "./ImageShow";
 
 const Images = (props) => {
   const images = props.images.map((image) => {
-    return <img key={image.id} src={image.urls.regular} />
-  })
+    return <ImageShow key={image.id} image={image} />;
+  });
 
-  return <div>{images}</div>
-}
+  return <div className="images">{images}</div>;
+};
 
 export default Images;
